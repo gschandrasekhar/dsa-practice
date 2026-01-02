012ea3c4ade9c93ab8750ab9be02ea0ce0ba385c
gschandrasekhar <gsivachandrasekhar.com>
2026-01-02 19:22:14 +0530

    02-01-2026

--- 04Loops/01forLoop.cpp
+++ 04Loops/01forLoop.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-   int num=10;
-   for(int i=0;i<10;i++)  //(intilaisation, condition, update/increment)
-   {
-    cout<<i<<" ,";
-   }
+  int num = 10;
+  for (int i = 0; i < 10; i++) //(intilaisation, condition, update/increment
+  {
+    cout << i << " ,";
+  }
   return 0;
 }
